085757ea97e4d1b4bd7f8d101590993b4a17bea0
takurinton <taryochanmannn@icloud.com>
2021-05-29 15:04:28 +0900

    fix: post

--- src/routes/post/[id].svelte
+++ src/routes/post/[id].svelte
@@ -2,58 +2,9 @@
 	import { enhance } from '$lib/form';
     import type { Load } from '@sveltejs/kit';
     import marked from 'marked';
-    import highlightjs from 'highlight.js';
+    import { syntaxHighlight, markdownStyle } from './utils.ts';
     
     export const prerender = true;
-
-    const syntaxHighlight = () => {
-        marked.setOptions({
-            highlight: (code:string, lang:string) => {
-                return highlightjs.highlightAuto(code, [lang]).value;
-            },
-        })
-    }
-    
-    const markdownStyle = () => {
-        const r = new marked.Renderer()
-            r.heading = (text:string, level:number) => {
-                return (
-                    `<h${level} class="h${level}"">${text}</h${level}>`
-                )
-            }
-            r.table = (header: string, body: string) => { 
-                return (
-                    `<table class="table" border="1" width="100%">
-                        ${header}
-                        ${body}
-                    </table>`
-                )
-            }
-            r.list = (body: string, ordered: boolean, start: number) => {
-                return (
-                    `
-                    <ul>
-                        ${body}
-                    </ul>
-                    `
-                )
-            }
-            r.image = (href: string | null, title: string | null, text: string) => {
-                return (
-                    `
-                    <img src=${href} class="content-img" />
-                    `
-                )
-            }
-            r.paragraph = (text: string) => {
-                return (
-                    `
-                    <p class="p">${text}</p>
-                    `
-                )
-            }
-        return r
-    }
 
     // getInitialProps 的なノリのやつ
     // page はページとかクエリパラメータとかを取得できる
