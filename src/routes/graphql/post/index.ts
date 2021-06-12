3ba6dd0822a531ecbecb8b74167c81c7828e5a2d
takurinton <taryochanmannn@icloud.com>
2021-06-12 11:23:38 +0900

    add: post

---
+++ src/routes/graphql/post/index.ts
@@ -0,0 +1,19 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client/core';
+import { POST_QUERY } from '../query';
+
+export const get = async (req) => {
+  const id = req.query.get('id');
+  const client = new ApolloClient({
+      uri: 'http://localhost:8080/graphql',
+      cache: new InMemoryCache()
+  });
+  
+  const props = await client.query({
+    query: POST_QUERY, 
+    variables: { id }
+  })
+
+  return {
+    body: props,
+  };
+}
